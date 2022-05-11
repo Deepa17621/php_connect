229e0fb3673b14af015e911f00a1ffe226697615
Deepa17621 <101962890+Deepa17621@users.noreply.github.com>
2022-05-11 14:06:00 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,16 +2,16 @@
 
 $fn=$_POST['First_Name'];
 $ln=$_POST['Last_Name'];
-$g=$_Post['Gender'];
+$g=$_POST['Gender'];
 $dob=$_POST['start-trip'];
-$pn=$_Post['Mobile'];
-$AaN=$_Post['Aadhar.No'];
-$em=$_Post['Email'];
-$Add=$_Post['Address'];
-$C=$_Post['city'];
-$pin=$_Post['pincode'];
-$pw=$_Post['Password'];
-$cpw=$_Post['confirmPassword'];
+$pn=$_POST['Mobile'];
+$AaN=$_POST['Aadhar.No'];
+$em=$_POST['Email'];
+$Add=$_POST['Address'];
+$C=$_POST['city'];
+$pin=$_POST['pincode'];
+$pw=$_POST['Password'];
+$cpw=$_POST['confirmPassword'];
 $con=mysqli_connect("localhost","root","","form-validation");
 $sql="INSERT INTO form-data (First Name,Last Name, Gender, Mobile No, Aadhar No,  Email, Address,  City, 
 Pincode,Password, Confirm Password) 
